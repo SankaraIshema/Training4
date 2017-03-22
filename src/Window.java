0e4f5f84b38adeae77d8690b1fa7d4861fedad89
Sankara <sankara.ishema@gmail.com>
2017-03-22 18:49:31 +0100

    Addition of ActionListners, use of intern classes and anonymous classes.
    However a problem with threads and infinite loop arose. I still don't
    know how to fix this, but as everything else, it shall be fixed.
    I know the answer lie somewhere between Middle-Earth and the Mordor, but
    my precious won't elude me for long. No, not for long...My Precious...

--- src/Window.java
+++ src/Window.java
@@ -1,25 +1,48 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Window extends JFrame{
+public class Window extends JFrame {
 
 	private Panel pan = new Panel();
 	private JPanel motherPane = new JPanel();
-	private Button button = new Button("Nice Button");
+	private JPanel buttonPane = new JPanel();
+	private JButton button1 = new JButton("Go");
+	private JButton button2 = new JButton("Stop");
+	private JLabel label = new JLabel("Nice Label");
+	private boolean greenLight = true;
 	
 	public Window() {
 		this.setTitle("Interesting window");
 		this.setSize(300, 300);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		motherPane.setBackground(Color.WHITE);
 		motherPane.setLayout(new BorderLayout());
-		motherPane.add(button, BorderLayout.SOUTH);
 		motherPane.add(pan, BorderLayout.CENTER);
+
+		buttonPane.add(button1);
+		buttonPane.add(button2);
+		motherPane.add(buttonPane, BorderLayout.SOUTH);
+		
+		Font policy = new Font("Tahoma", Font.BOLD, 16);
+		label.setFont(policy);
+		label.setForeground(Color.BLUE);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		motherPane.add(label, BorderLayout.NORTH);
+		
+		button1.addActionListener(new ButtonListener1());
+		button1.setEnabled(false);
+		button2.addActionListener(new ButtonListener2());
+		
 		this.setContentPane(motherPane);
 		this.setVisible(true);
 		Go();
@@ -34,7 +57,7 @@ public class Window extends JFrame{
 		Boolean backX = true;
 		Boolean backY = true;
 		
-		while(true) {
+		while(this.greenLight) {
 			
 			/* When the ball is going forward backX is true 
 			 * When the ball is going backward backX is false */
@@ -74,6 +97,28 @@ public class Window extends JFrame{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}	
+		}
+	}
+	
+	class ButtonListener1 implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			greenLight = true;
+			button1.setEnabled(false);
+			button2.setEnabled(true);
+			Go();
+		}	
+	}
+	
+	class ButtonListener2 implements ActionListener {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			greenLight = false;
+			button1.setEnabled(true);
+			button2.setEnabled(false);
+				
 		}
 	}
 }
